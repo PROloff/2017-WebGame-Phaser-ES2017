1a3b5d261e0e69c4cb985c691117d0b4ad6c9ab0
PROloff <proloff@lspb.de>
2017-09-29 22:40:27 +0200

    merge

--- dist/bundle.js
+++ dist/bundle.js
@@ -10970,10 +10970,10 @@ var _class = function (_Phaser$State) {
       //
       // load your assets
       //
-      // this.load.image('pit', 'assets/images/pit.png');
-      // this.load.image('bullet', 'assets/images/bullet.png');
-      // this.load.image('ship', 'assets/images/ship.png');
-      // this.load.image('smoke', 'assets/images/smoke.png');
+      //this.load.image('pit', 'assets/images/pit.png');
+      //this.load.image('bullet', 'assets/images/bullet.png');
+      //this.load.image('ship', 'assets/images/ship.png');
+      //this.load.image('smoke', 'assets/images/smoke.png');
       this.load.image('bullet1', 'assets/images/bullet(1)');
       this.load.spritesheet('dude', 'assets/images/dude.png', 32, 48);
       this.load.image('background', 'assets/images/background.jpg');
