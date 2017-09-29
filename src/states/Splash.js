9792b212e2d6330b37d9d6fd4d258721770cf59f
PROloff <proloff@lspb.de>
2017-09-29 22:21:05 +0200

    m

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -11,18 +11,13 @@ export default class extends Phaser.State {
     //
     // load your assets
     //
-    this.load.image('pit', 'assets/images/pit.png');
-    this.load.image('bullet', 'assets/images/bullet.png');
-    this.load.image('ship', 'assets/images/ship.png');
-    this.load.image('smoke', 'assets/images/smoke.png');
-<<<<<<< HEAD
-    this.load.image('dude','assets/images/dude.png' );
-    this.load.image('background', 'assets/images/background.jpg');
+   // this.load.image('pit', 'assets/images/pit.png');
+   // this.load.image('bullet', 'assets/images/bullet.png');
+   // this.load.image('ship', 'assets/images/ship.png');
+   // this.load.image('smoke', 'assets/images/smoke.png');
     this.load.image('bullet1', 'assets/images/bullet(1)');
-=======
     this.load.spritesheet('dude','assets/images/dude.png', 32, 48);
     this.load.image('background', 'assets/images/background.jpg');
->>>>>>> 4a75ee5a851bc0e274379df14732664c7479d38a
   }
 
   create () {
