a5938348eb9304ae4b1d3214791219e53b99c9a3
PROloff <proloff@lspb.de>
2017-09-29 21:01:50 +0200

    mergee

--- src/states/GameOver.js
+++ src/states/GameOver.js
@@ -5,11 +5,7 @@ import Ship from '../sprites/Ship';
 export default class extends Phaser.State {
 
   create () {
-<<<<<<< HEAD
     this.gameOver = this.add.text(0, 40, 'Winner');
-=======
-    this.gameOver = this.add.text(0, 40, 'ok');
->>>>>>> f1b1818a9fa7bb5b5b1f6868075a84d26bf1af62
     this.gameOver.font = 'Bangers';
     this.gameOver.padding.set(10, 16);
     this.gameOver.fontSize = 100;
