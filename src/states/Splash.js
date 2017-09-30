7065bb445e686ba20f605458959f294ce7d84afa
PROloff <proloff@lspb.de>
2017-09-30 10:31:56 +0200

    merge

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -16,7 +16,7 @@ export default class extends Phaser.State {
     //this.load.image('ship', 'assets/images/ship.png');
     //this.load.image('smoke', 'assets/images/smoke.png');
     this.load.image('bullet1', 'assets/images/bullet(1)');
-    this.load.spritesheet('dude','assets/images/dude.png', 32, 48);
+    this.load.spritesheet('dude','assets/images/dude2.png', 32, 48);
     this.load.image('background', 'assets/images/background.jpg');
   }
 
