460eacf46d50b18e0a7ce786d61b44be66f9850d
PROloff <proloff@lspb.de>
2017-10-01 12:52:35 +0200

    merge

--- dist/bundle.js
+++ dist/bundle.js
@@ -11202,7 +11202,7 @@ var _class = function (_Phaser$State) {
       this.bg = game.add.tileSprite(0, 0, window.innerWidth, window.innerHeight, 'Blue');
       var currentTime = new Date().getTime();
 
-      while (currentTime + 1000 >= new Date().getTime()) {}
+      while (currentTime + 1500 >= new Date().getTime()) {}
       this.end = game.add.audio('End');
       this.end.play();
     }
@@ -11258,7 +11258,7 @@ var _class = function (_Phaser$State) {
       this.bg = game.add.tileSprite(0, 0, window.innerWidth, window.innerHeight, 'Red');
       var currentTime = new Date().getTime();
 
-      while (currentTime + 1000 >= new Date().getTime()) {}
+      while (currentTime + 1500 >= new Date().getTime()) {}
       this.end = game.add.audio('End');
       this.end.play();
     }
