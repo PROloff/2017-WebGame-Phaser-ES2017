6f85a54e150db7f970efd0ef0aaea36781b065d8
PROloff <proloff@lspb.de>
2017-10-01 12:26:35 +0200

    m

--- dist/bundle.js
+++ dist/bundle.js
@@ -11385,7 +11385,6 @@ var _class = function (_Phaser$State) {
       this.load.image('bulletB', 'assets/images/bulletBlau.png');
       this.load.spritesheet('dude', 'assets/images/dudeRot_klein.png', 64, 64);
       this.load.image('background', 'assets/images/stadt3.jpg');
-
       this.load.tilemap('map', 'assets/map/Level-Retro.json', null, _phaserCe2.default.Tilemap.TILED_JSON);
       this.load.image('Tileset1', 'assets/map/retro.png');
       this.load.spritesheet('dude2', 'assets/images/dudeBlau_klein.png', 64, 64);
